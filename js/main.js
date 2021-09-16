96f77cbab7159dec4aa3bb363907119af0e15c56
khusniddin-akhmadjanov <husniddin1005@inbox.ru>
2021-09-16 23:15:19 +0500

    Add input back style

--- js/main.js
+++ js/main.js
@@ -15,28 +15,39 @@ var elInputNumber = document.querySelector('.examBall').value.trim();
 
 var elInputCheck = document.querySelector('.checkForm');
 
-if (elInputCheck.checked) {
+if (elInputNumber < 0) {
+  document.querySelector('.examResultOutput').textContent = "0 va undan katta son kiriting!";
+  document.querySelector('.examResultOutput').style.backgroundColor = "black";
+}
+
+else if (elInputCheck.checked) {
   document.querySelector('.examResultOutput').textContent = "Imtiyozli qabul qilindingiz!";
+  document.querySelector('.examResultOutput').style.backgroundColor = "yellow";
 }
 
 else if(elInputNumber <= yiqildi) {
   document.querySelector('.examResultOutput').textContent = "Afsus imtihondan o`ta olmadingiz :(";
+  document.querySelector('.examResultOutput').style.backgroundColor = "red";
 }
 
 else if( yiqildi <= elInputNumber && elInputNumber <= supper) {
-  document.querySelector('.examResultOutput').textContent = "Siz supper kontrak asosida qabul qilindingiz";
+  document.querySelector('.examResultOutput').textContent = "Siz supper kontrak asosida qabul qilindingiz";  document.querySelector('.examResultOutput').style.backgroundColor = "red";
+  document.querySelector('.examResultOutput').style.backgroundColor = "orangered";
 }
 
 else if( supper <= elInputNumber && elInputNumber <= kontrakt) {
   document.querySelector('.examResultOutput').textContent = "Siz kontrak asosida qabul qilindingiz";
+  document.querySelector('.examResultOutput').style.backgroundColor = "orange";
 }
 
 else if ( kontrakt <= elInputNumber && elInputNumber <= budjet) {
   document.querySelector('.examResultOutput').textContent = "Siz grand asosida qabul qilindingiz";
+  document.querySelector('.examResultOutput').style.backgroundColor = "green";
 }
 
 else {
-  document.querySelector('.examResultOutput').textContent = "Mavjud bo`lmagan raqam kiritildi";
+  document.querySelector('.examResultOutput').textContent = "Raqam kiriting!";
+  document.querySelector('.examResultOutput').style.backgroundColor = "blue";
 }
 
 });
